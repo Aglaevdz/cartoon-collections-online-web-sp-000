e80eca7b19d57ab2eef56f8a04097bb6dd66bf6b
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-23 01:21:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(calls_long)# code an argument here
-  calls_long.any? do {|word| word.length > 4 }
+  calls_long.any? do |word| word.length > 4 
     return true
   end
   #end
